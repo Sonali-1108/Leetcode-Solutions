194d35d051c8797c86f78cff61a8d37d5741b25f
sonali_11 <231890563+Sonali-1108@users.noreply.github.com>
2026-08-31 21:06:54 +0530

    Time: 5 ms (82.17%), Space: 45.8 MB (87.03%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int original = x;
+        int result = 0;
+        int remainder;
+
+        while(x > 0){
+        remainder = x%10;
+        result = result*10+remainder;
+        x=x/10;
+        }
+        return result == original;
+    }
+}
