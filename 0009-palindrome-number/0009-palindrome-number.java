4b5348c4074201c9e15fe1364e1bedc59ceda9ca
sonali_11 <231890563+Sonali-1108@users.noreply.github.com>
2026-08-31 21:07:47 +0530

    Time: 5 ms (82.17%), Space: 45.6 MB (94.04%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0){
+        if (x < 0) {
             return false;
         }
         int original = x;
         int result = 0;
         int remainder;
 
-        while(x > 0){
-        remainder = x%10;
-        result = result*10+remainder;
-        x=x/10;
+        while (x > 0) {
+            remainder = x % 10;
+            result = result * 10 + remainder;
+            x = x / 10;
         }
         return result == original;
     }
