7f54541f84992b764d48b5c95acf9f38d3c1878b
sonali_11 <231890563+Sonali-1108@users.noreply.github.com>
2026-09-03 21:53:06 +0530

    Time: 430 ms (5.26%), Space: 47.7 MB (57.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        for(int j=0;j<nums.length;j++)
-        {
-        for(int i =1;i<nums.length;i++)
-        {
-            if(nums[i-1] == 0){
-                int temp = nums[i-1];
-                nums[i-1] = nums[i];
-                nums[i] = temp;
+        for (int j = 0; j < nums.length; j++) {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i - 1] == 0) {
+                    int temp = nums[i - 1];
+                    nums[i - 1] = nums[i];
+                    nums[i] = temp;
+                }
+
             }
-            
-        }
         }
     }
 }
